c48c2005f24f0c61a31d754099eff33aa342f2b4
ichaehun <ichaehunn@gmail.com>
2022-09-03 00:48:07 +0900

    문제가있는 반복문

--- src/demo/step1/Main2.java
+++ src/demo/step1/Main2.java
@@ -4,7 +4,19 @@ import java.util.Scanner;
 
 public class Main2 {
     public static void main(String[] args) {
+        while (true) {
+            Scanner yourName = new Scanner(System.in);
+            System.out.println("Enter your name : ");
 
+            String a = yourName.next();
 
+            if(yourName.next() == "exit"){
+                break;
+            }
+            else {
+                System.out.println("hi~ " + a);
+            }
+        }
+        System.out.println("종료");
     }
 }
