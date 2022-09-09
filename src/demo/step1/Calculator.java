e6bf70874f04a78587ba0e7ab0345a02240c14a6
ichaehun <ichaehunn@gmail.com>
2022-09-09 11:23:02 +0900

    사칙연산 문제

--- src/demo/step1/Calculator.java
+++ src/demo/step1/Calculator.java
@@ -6,29 +6,30 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("java Calculator : ");
+        while (true) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            String c = scan.next();
 
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        String c = scan.next();
+            switch (c) {
+                case "+":
+                    System.out.println(a + b);
+                    break;
+                case "-":
+                    System.out.println(a - b);
+                    break;
+                case "*":
+                    System.out.println(a * b);
+                    break;
+                case "/":
+                    System.out.println(a / b);
+                    break;
+                default:
+                    System.out.println("사칙연산을 잘못입력하셨습니다");
+                    break;
+            }
 
-        switch(c){
-            case "+":
-                System.out.println(a+b);
-                break;
-            case "-":
-                System.out.println(a-b);
-                break;
-            case "*":
-                System.out.println(a*b);
-                break;
-            case "/":
-                System.out.println(a/b);
-                break;
-            default:
-                System.out.println("사칙연산을 잘못입력하셨습니다");
-                break;
         }
+
     }
-
-
 }
